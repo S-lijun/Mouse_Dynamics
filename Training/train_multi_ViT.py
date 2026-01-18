be218db3dd171c78cd525b3b290e6be0c078d8d8
S-lijun <jayden@wustl.edu>
2026-01-18 17:20:22 -0600

    /

--- Training/train_multi_ViT.py
+++ Training/train_multi_ViT.py
@@ -117,8 +117,7 @@ if __name__ == "__main__":
     
     Images = ["Chunk/Balabit_chunks_XY_/event60","Chunk/Balabit_chunks_XY_/event30"]
     Images = ["Chunk/Balabit_chunks_XY_/event300","Chunk/Balabit_chunks_XY_/event120"]
-    Images = ["Chunk/Balabit_chunks_XY_/event300","Chunk/Balabit_chunks_XY_/event120","Chunk/Balabit_chunks_XY_/event60",
-              "Chunk/Balabit_chunks_XY_/event30"]
+    Images = ["Chunk/Balabit_chunks_XY_/event60", "Chunk/Balabit_chunks_XY_/event30"]
     
     #Images = ["Chunk/Balabit_chunks_cdf/event15", "Chunk/Balabit_chunks_cdf/event10"]
     
