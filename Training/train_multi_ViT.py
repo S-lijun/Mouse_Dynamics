f8f8059ceb5136a6e4c8c8a71507bc8a0f74cb25
S-lijun <jayden@wustl.edu>
2026-01-18 18:54:23 -0600

    /

--- Training/train_multi_ViT.py
+++ Training/train_multi_ViT.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
             model, best_model, *_ = trainer.train(
                 optim_name='sgd', # "sgd or adam" 
                 num_epochs=25,
-                learning_rate=0.01,
+                learning_rate=0.001,
                 step_size=7,
                 learning_rate_decay=0.1,
                 acc_frequency=1,
