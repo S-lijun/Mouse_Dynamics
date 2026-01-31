c2e3fc0461c8ccddfd6dbfdeff6e1b8892785c51
S-lijun <jayden@wustl.edu>
2026-01-31 14:08:24 -0600

    /

--- ImageGeneration/RecurrencePlot/SRP.py
+++ ImageGeneration/RecurrencePlot/SRP.py
@@ -24,12 +24,14 @@ DATA_ROOT = os.path.join(ROOT, "Data", "Balabit-dataset", "training_files")
 print(f"[AutoRoot] Project root detected = {ROOT}")
 print(f"[AutoRoot] Using data_dir = {DATA_ROOT}")
 
+
 # ----------------------------
 # Drawing Utils
 # ----------------------------
 IMG_SIZE = (224, 224)
 IMG_SIZE = (np.sqrt(10*224*224),np.sqrt(10*224*224))
 IMG_SIZE = (np.sqrt(4*224*224),np.sqrt(4*224*224))
+IMG_SIZE = (np.sqrt(2*224*224),np.sqrt(2*224*224))
 
 DPI = 100
 
@@ -200,7 +202,7 @@ def parse_args():
     p = argparse.ArgumentParser(
         description="SRP generation with unified style (Balabit)"
     )
-    p.add_argument("--out_dir", type=str, default="Images/pixel_vs_chunk_SRP/event_120")
+    p.add_argument("--out_dir", type=str, default="Images/pixel_vs_chunk_SRP/event_60")
     p.add_argument("--sizes", type=int, nargs="+", default=[10, 15, 30, 60, 120, 300])
     p.add_argument("--users", type=str, nargs="+", default=[])
     p.add_argument("--sessions", type=str, nargs="+", default=[])
