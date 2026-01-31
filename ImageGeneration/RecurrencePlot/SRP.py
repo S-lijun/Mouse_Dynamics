ead7414c85d5e65e648b5949cdfd507e2727c8dc
S-lijun <jayden@wustl.edu>
2026-01-31 05:10:04 -0600

    /

--- ImageGeneration/RecurrencePlot/SRP.py
+++ ImageGeneration/RecurrencePlot/SRP.py
@@ -28,6 +28,8 @@ print(f"[AutoRoot] Using data_dir = {DATA_ROOT}")
 # Drawing Utils
 # ----------------------------
 IMG_SIZE = (224, 224)
+IMG_SIZE = (np.sqrt(10*224*224),np.sqrt(10*224*224))
+
 DPI = 100
 
 
@@ -197,7 +199,7 @@ def parse_args():
     p = argparse.ArgumentParser(
         description="SRP generation with unified style (Balabit)"
     )
-    p.add_argument("--out_dir", type=str, default="Images/Chunk/Balabit_chunks_SRP_black_white/training")
+    p.add_argument("--out_dir", type=str, default="Images/pixel_vs_chunk_SRP/event_300")
     p.add_argument("--sizes", type=int, nargs="+", default=[10, 15, 30, 60, 120, 300])
     p.add_argument("--users", type=str, nargs="+", default=[])
     p.add_argument("--sessions", type=str, nargs="+", default=[])
--- Training/LOSO/train_multi_CNN_82.py
+++ Training/LOSO/train_multi_CNN_82.py
@@ -251,7 +251,7 @@ if __name__ == "__main__":
             _, best_model, *_ = trainer.train(
                 optim_name="adamw",
                 num_epochs=17,
-                learning_rate=0.001,
+                learning_rate=0.0001,
                 step_size=5,
                 learning_rate_decay=0.1,
                 verbose=True
