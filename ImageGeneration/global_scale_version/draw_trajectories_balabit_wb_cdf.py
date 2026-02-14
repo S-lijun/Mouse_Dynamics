cebb47cf5aadb079b97d59c3be8f776dd7c4dbaa
S-lijun <jayden@wustl.edu>
2026-02-13 19:09:10 -0600

    /

--- ImageGeneration/global_scale_version/draw_trajectories_balabit_wb_cdf.py
+++ ImageGeneration/global_scale_version/draw_trajectories_balabit_wb_cdf.py
@@ -20,6 +20,7 @@ from scipy.stats import rankdata
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "../.."))
 # 默认路径根据你的目录结构调整
 DATA_ROOT = os.path.join(ROOT, "Data", "Balabit-dataset", "training_files")
+DATA_ROOT = os.path.join(ROOT, "Data", "Balabit-dataset", "testing_files_protocol1")
 
 # Fixed canvas and stroke parameters
 FIXED_IMG_SIZE = 448
@@ -186,7 +187,7 @@ def main():
     global GLOBAL_V_ALL, GLOBAL_CDF_ALL
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("--out_dir", type=str, default=f"Images/fixed_{FIXED_IMG_SIZE}padding_wb_cdf")
+    parser.add_argument("--out_dir", type=str, default=f"Images/fixed_{FIXED_IMG_SIZE}padding_wb_cdf_protocol1")
     parser.add_argument("--sizes", type=int, nargs="+", default=[15, 30, 60, 120, 300])
     parser.add_argument("--clip", type=float, default=95.0, help="Percentile to clip global velocity")
     parser.add_argument("--users", type=str, nargs="+", default=[])
