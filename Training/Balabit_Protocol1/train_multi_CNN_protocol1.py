aca6a75009a8192a5d4a4550b08fb13a8b3bd6b2
S-lijun <jayden@wustl.edu>
2026-02-17 18:52:18 -0600

    /

--- Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
+++ Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
@@ -136,8 +136,8 @@ if __name__ == "__main__":
     print("[INFO] Using device:", device)
     
     # path
-    training_folder = "XYPLot_cdf/event60"
-    testing_folder  = "XYPLot_cdf_protocol1/event60"
+    training_folder = "SRP_224/event60"
+    testing_folder  = "SRP_224_protocol1/event60"
     
     img_size = 224
     C_pos, C_neg = 60, 60
