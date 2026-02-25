8e34f017434aa8acda150783af4d848964c1b439
S-lijun <jayden@wustl.edu>
2026-02-24 22:17:49 -0600

    /

--- Training/Balabit_Protocol2/train_multi_CNN_protocol2.py
+++ Training/Balabit_Protocol2/train_multi_CNN_protocol2.py
@@ -124,8 +124,8 @@ if __name__ == "__main__":
 
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    training_folder = "XYPlot/event60"
-    testing_folder  = "XYPlot_protocol2"
+    training_folder = "XYPLot_cdf/event60"
+    testing_folder  = "XYPLot_cdf_protocol2"
     img_size = 224
 
     train_root = Path(project_root) / "Images" / training_folder
