f821df85dff494302187a2e791ce646e0b905770
S-lijun <jayden@wustl.edu>
2026-02-27 23:15:16 -0600

    /

--- Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
+++ Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
@@ -136,10 +136,10 @@ if __name__ == "__main__":
     print("[INFO] Using device:", device)
 
     # path
-    training_folder = "SRP_time/event60"
-    testing_folder  = "SRP_time_protocol1/event60"
+    training_folder = "SRP/event60"
+    testing_folder  = "SRP_protocol1/event60"
     
-    img_size = 300
+    img_size = 224
     C_pos, C_neg = 60, 60
     
     train_root = Path(project_root) / "Images" / training_folder
