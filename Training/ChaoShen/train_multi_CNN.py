5aecd0d598394b5d3f8bbdd0c84eb94d06acd9a5
S-lijun <jayden@wustl.edu>
2026-03-10 07:15:57 -0800

    /

--- Training/ChaoShen/train_multi_CNN.py
+++ Training/ChaoShen/train_multi_CNN.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
         train_dataset,
         batch_size=128,
         shuffle=True,
-        num_workers=12,
+        num_workers=2,
         pin_memory=True,
         persistent_workers=True
     )
@@ -180,7 +180,7 @@ if __name__ == "__main__":
         test_dataset,
         batch_size=128,
         shuffle=False,
-        num_workers=12,
+        num_workers=2,
         pin_memory=True,
         persistent_workers=True
     )
