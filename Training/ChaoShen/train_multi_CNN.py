18cf994e285b75c99f366ccc476d9079c2de2262
S-lijun <jayden@wustl.edu>
2026-03-09 19:57:36 -0500

    /

--- Training/ChaoShen/train_multi_CNN.py
+++ Training/ChaoShen/train_multi_CNN.py
@@ -169,18 +169,18 @@ if __name__ == "__main__":
    
     train_loader = DataLoader(
     train_dataset,
-    batch_size=256,
+    batch_size=128,
     shuffle=True,
-    num_workers=12,
+    num_workers=16,
     pin_memory=True,
     persistent_workers=True
     )
 
     test_loader = DataLoader(
         test_dataset,
-        batch_size=256,
+        batch_size=128,
         shuffle=False,
-        num_workers=12,
+        num_workers=16,
         pin_memory=True,
         persistent_workers=True
     )
