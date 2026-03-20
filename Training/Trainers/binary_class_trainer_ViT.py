ad69edfff92eb5074beb8a39e749d128f3162f1c
S-lijun <jayden@wustl.edu>
2026-03-19 18:13:53 -0800

    /

--- Training/Trainers/binary_class_trainer_ViT.py
+++ Training/Trainers/binary_class_trainer_ViT.py
@@ -364,7 +364,7 @@ class BinaryClassTrainer:
         # Early stopping
         # ====================================================
 
-        patience = 15
+        patience = 5
         patience_counter = 0
 
         train_losses = []
