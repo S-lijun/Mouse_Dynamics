23c1af79a514ba6482da37eafee0b05a8c6f0444
S-lijun <jayden@wustl.edu>
2026-03-21 00:28:45 -0800

    /

--- Training/ChaoShen/train_multi_CNN.py
+++ Training/ChaoShen/train_multi_CNN.py
@@ -220,10 +220,10 @@ if __name__ == "__main__":
 
     _, best_model, *_ = trainer.train(
         optim_name="adamw",
-        num_epochs=30,
-        learning_rate=0.00005,
-        step_size=7,
-        learning_rate_decay=0.5,
+        num_epochs=17,
+        learning_rate=0.0001,
+        step_size=5,
+        learning_rate_decay=0.1,
         verbose=True
     )
 
