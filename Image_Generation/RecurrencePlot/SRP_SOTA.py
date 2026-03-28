083a7f8b1a2e7f7a55038f85feeb7e9572f5180e
S-lijun <jayden@wustl.edu>
2026-03-28 02:37:39 -0800

    /

--- Image_Generation/RecurrencePlot/SRP_SOTA.py
+++ Image_Generation/RecurrencePlot/SRP_SOTA.py
@@ -216,7 +216,7 @@ def main():
     parser.add_argument("--data_root", required=True)
     parser.add_argument("--out_dir", required=True)
     parser.add_argument("--sizes", type=int, nargs="+", default=[300])
-    parser.add_argument("--epsilon", type=float, default=1)
+    parser.add_argument("--epsilon", type=float, default=0.5)
 
     args = parser.parse_args()
 
