cfe66c526d095dd68057c4e61415fd35c50134ca
S-lijun <jayden@wustl.edu>
2026-03-29 22:15:28 -0800

    /

--- Training/Balabit_Protocol1/train_binary_ViT_protocol1.py
+++ Training/Balabit_Protocol1/train_binary_ViT_protocol1.py
@@ -197,7 +197,7 @@ if __name__ == "__main__":
         train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=8)
         test_loader  = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=8)
 
-        net = BinaryViT(img_size=img_size, patch_size=15, in_chans=1).to(device)
+        net = BinaryViT(img_size=img_size, patch_size=15, in_chans=3).to(device)
 
         trainer = BinaryClassTrainer(
             net=net,
