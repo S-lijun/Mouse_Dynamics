afb01bbf0f28a73084aea419a52175b1b8706f0c
S-lijun <jayden@wustl.edu>
2026-04-16 22:52:52 -0800

    /

--- Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
+++ Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
@@ -167,8 +167,8 @@ if __name__ == "__main__":
     test_dataset  = Protocol1MouseDataset(test_root, user_list, transform)
 
    
-    train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=4)
-    test_loader  = DataLoader(test_dataset, batch_size=64, shuffle=False, num_workers=4)
+    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers=4)
+    test_loader  = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
 
     print(f"[INFO] Train samples: {len(train_dataset)} | Test samples: {len(test_dataset)}")
 
