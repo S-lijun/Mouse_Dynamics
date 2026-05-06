bf7f77257afd3ca4971f8108d0f54618da0a6e2a
S-lijun <jayden@wustl.edu>
2026-05-06 02:32:14 -0800

    /

--- Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
+++ Training/Balabit_Protocol1/train_multi_CNN_protocol1.py
@@ -188,9 +188,9 @@ if __name__ == "__main__":
     print("\n========== Training Execution ==========")
     _, best_model, *_ = trainer.train(
         optim_name="adamw",
-        num_epochs=25,
-        learning_rate=0.001,
-        step_size=7,
+        num_epochs=22,
+        learning_rate=0.0001,
+        step_size=6,
         learning_rate_decay=0.1,
         verbose=True
     )
