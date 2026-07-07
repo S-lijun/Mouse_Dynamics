761d832c5af8582a18f802aa1f8e389fe91991c3
S-lijun <jayden@wustl.edu>
2026-07-06 19:01:19 -0500

    /

--- Training/ChaoShen/train_multi_ViT.py
+++ Training/ChaoShen/train_multi_ViT.py
@@ -183,7 +183,7 @@ if __name__ == "__main__":
 
     train_loader = DataLoader(
         train_dataset,
-        batch_size=64,
+        batch_size=128,
         shuffle=True,
         num_workers=12,
         pin_memory=True,
@@ -193,7 +193,7 @@ if __name__ == "__main__":
 
     test_loader = DataLoader(
         test_dataset,
-        batch_size=64,
+        batch_size=128,
         shuffle=False,
         num_workers=12,
         pin_memory=True,
